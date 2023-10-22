3633451c6295cec55a58119857b20fd9195c3fd7
Adurizz <90387438+Adurizz@users.noreply.github.com>
2023-10-22 20:57:44 +0900

    init
    init

---
+++ my_extension/content.js
@@ -0,0 +1,10 @@
+if (window.location.host === 'blog.naver.com') {
+    let blogContent = document.querySelector('.se-main-container');
+    if (blogContent) {
+        let textContent = blogContent.innerText;
+        console.log(textContent);
+        // 여기서 textContent 전처리&모델에 입력해서 결과 받아야함
+    } else {
+        console.log('No blog content found');
+    }
+}
