9527d5677d7352f14dbe36caf6ef680c17174409
jeonghyoan <goodhyoan32@gmail.com>
2023-10-22 21:51:17 +0900

    hihi

--- my_extension/content.js
+++ my_extension/content.js
@@ -4,6 +4,7 @@ if (window.location.host === 'blog.naver.com') {
         let textContent = blogContent.innerText;
         console.log(textContent);
         // 여기서 textContent 전처리&모델에 입력해서 결과 받아야함
+        // 히힛
     } else {
         console.log('No blog content found');
     }
