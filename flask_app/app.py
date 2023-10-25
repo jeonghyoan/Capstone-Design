2cec31e1077862965802d4100f487b6664dc3d85
jeonghyoan <goodhyoan32@gmail.com>
2023-10-25 11:03:37 +0900

    에러해결!

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from classification_module import ClassificationSystem
 
+
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
--- flask_app/classification_module.py
+++ flask_app/classification_module.py
@@ -29,6 +29,7 @@ import cv2
 import requests
 import sys
 
+
 class ClassificationSystem:
     def __init__(self, reloadedModel = None, contents = None, threshold = 0.5, savedModelPath = None, excludeWordList = None, keyWordList = None, lastNum = 5):
         self.reloadedModel = reloadedModel
