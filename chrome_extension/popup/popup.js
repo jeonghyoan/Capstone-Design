ad7f6cba5a1c0f03eb5666261d77eb2a2cc51a77
Adurizz <nickhs988@gmail.com>
2023-11-01 19:44:22 +0900

    업데이트 시점 변경

--- chrome_extension/popup/popup.js
+++ chrome_extension/popup/popup.js
@@ -1,15 +1,26 @@
 window.onload = function () {
-	var resultText = document.getElementById("result");
+    var resultText = document.getElementById("result");
 
-	chrome.runtime.sendMessage({action: "sendResult"}, function(response) {
-		let result = response.result;
-		if (result == -1) {
-			resultText.textContent = '페이지를 볼 수 없음';
-		} else if (result == 0) {
-			resultText.textContent = '이 거친세상속 믿을만한리뷰ㅠ';
-		} else if (result == 1) {
-			resultText.textContent = '광곤데 믿었니?ㅋ';
-		}
-   });
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var tab = tabs[0]; // 현재 활성화된 탭을 가져옵니다.
+        fetch('http://127.0.0.1:5000/classify', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ link: tab.url }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            let result = data.result;
+            if (result == -1) {
+                resultText.textContent = '페이지를 볼 수 없음';
+            } else if (result == 0) {
+                resultText.textContent = '이 거친세상속 믿을만한리뷰ㅠ';
+            } else if (result == 1) {
+                resultText.textContent = '광곤데 믿었니?ㅋ';
+            } else {
+				resultText.textContent = '네이버 블로그인지 확인 부탁드립니다';
+			}
+        })
+        .catch(console.error);
+    });
 };
-
